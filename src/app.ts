e7d9487189714698dd58e47bf1f21e5b85ab49bb
m4cone <boffmarcone@gmail.com>
2025-06-14 14:53:36 -0300

    feat: add fastityJwt register

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 import fastify from "fastify";
-import { routes } from "./http/routes";
+import cookie from "@fastify/cookie";
+import { userRoutes } from "./http/controllers/users/routes";
 import { errorHandlerResponse } from "./infra/controller";
 import { ZodError } from "zod";
 import { ValidationError } from "./infra/errors";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 
-app.register(routes);
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refresh_token",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(cookie);
+
+app.register(userRoutes);
+app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _req, res) => {
   if (error instanceof ZodError) {
